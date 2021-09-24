5429ba2a5179602d538328e592560a8635c5d3cb
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-09-24 23:20:31 +0600

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -10,7 +10,7 @@ if SN == nil then else
 if SN==1 then F0() end
 if SN==2 then F1() end
 if SN==3 then F2() end
-if SN==3 then F3() end
+if SN==4 then F3() end
 end 
   PUBGMH = -1
 end
@@ -58,7 +58,7 @@ end
 
 
 function F3()
-local﻿ file = gg.makeRequest('#')
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/script1.6/main/HOME.lua')
 if file.content == nil or file.content == '' then
 os.exit()
 end﻿
