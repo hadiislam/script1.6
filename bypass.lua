8831b0b369eab98bfc54ceec591489a405f11f96
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-09-24 22:42:55 +0600

    Add files via upload

---
+++ bypass.lua
@@ -0,0 +1,77 @@
+
+function HOME()
+SN = gg.choice({ 
+   "🛡️ 3ʀᴅ ᴘᴀʀᴛʏ ꜰɪx ( ʟᴏɢᴏ )",
+   "🛡️ ᴍɪꜱᴀᴋɪ ʙʏᴘᴀꜱꜱ",
+   "🛡️ 7ʜ ʙʏᴘᴀꜱꜱ",
+   "🛑 ʙᴀᴄᴋ ",
+	 }, nil, "●▬▬▬▬▬▬▬▬▬▬▬▬●\n• ꜱᴄʀɪᴘᴛ ʙʏ : ʜᴀᴅɪ ɪꜱʟᴀᴍ 🇧🇩\n• ᴛᴇʟᴇɢʀᴀᴍ : ʜᴀᴅɪ_ɪꜱʟᴀᴍ 🇧🇩\n• ᴘᴜʙɢ ᴠᴇʀꜱɪᴏɴ : 1.6 \n• ☠️ ᴜꜱᴇ ᴀᴛ ʏᴏᴜʀ ᴏᴡɴ ʀɪꜱᴋ ☠️\n●▬▬▬▬▬▬▬▬▬▬▬▬●\n️")
+if SN == nil then else
+if SN==1 then F0() end
+if SN==2 then F1() end
+if SN==3 then F2() end
+if SN==3 then F3() end
+end 
+  PUBGMH = -1
+end
+
+
+
+
+
+function F0()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/script1.6/main/3rd%20party.lua')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+
+function F1()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/script1.6/main/misaki%20bypass.lua')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+function F2()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/script1.6/main/7H.lua')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+
+function F3()
+local﻿ file = gg.makeRequest('#')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+
+
+HOME()
+
+
+
