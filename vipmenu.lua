9c6e08067dd1d49449cc77bf2bcb6d02dfeb140a
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-09-29 03:22:16 +0600

    Create vipmenu.lua

---
+++ vipmenu.lua
@@ -0,0 +1,69 @@
+function HOME()
+SN = gg.choice({
+"( ʟᴏɢᴏ ᴋʀᴀꜰᴛᴏɴ )\n🧩 𝙇𝙊𝙂𝙊 𝘽𝙔𝙋𝘼𝙎𝙎",
+    "( ʟᴏʙʙʏ )\n🛡 𝘽𝙔𝙋𝘼𝙎𝙎 ",
+    "( ɪɴ ɢᴀᴍᴇ )\n🔧 𝙊𝙉𝙀 𝘾𝙇𝙄𝘾𝙆 𝙃𝘼𝘾𝙆 ",
+    "( ᴇɴᴅ ɢᴀᴍᴇ )\n🎯 𝘾𝙇𝙀𝘼𝙍 𝙇𝙊𝙂",
+    "🚫 ᴇxɪᴛ "
+	 }, nil, "●▬▬▬▬▬▬▬▬▬▬▬▬●\n╔➤ᴛᴇʟᴇɢʀᴀᴍ : @ɪᴏʀɪʜᴀᴄᴋ\n╠➤ᴏᴡɴᴇʀ : @xᴅʙʟᴀsᴛᴇʀ\n╚➤ ғʀᴇᴇ ʜᴀᴄᴋs ᴏɴʟʏ ●▬▬▬▬▬▬▬▬▬▬▬▬●\n️")
+if SN == nil then else
+if SN==1 then LOGO() end
+if SN==2 then BYPASS() end
+if SN==3 then ONECLICK() end
+if SN==4 then LOG() end
+if SN==5 then Exit() end
+end 
+  PUBGMH = -1
+end
+
+function LOGO()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/SafeScriptgg/main/GG')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+function BYPASS()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/SafeScriptgg/main/GG')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+function ONECLICK()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/SafeScriptgg/main/GG')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+function LOG()
+local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/SafeScriptgg/main/GG')
+if file.content == nil or file.content == '' then
+os.exit()
+end﻿
+local load =  load(file.content)
+if load == nil﻿﻿ then
+os.exit()
+end 
+load()
+end
+
+function EXIT()
+os.exit()
+end
