48c7b24e70825531f9160f4bb9b486b7f414da61
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-09-29 03:28:39 +0600

    Create viplogo.lua

---
+++ viplogo.lua
@@ -0,0 +1,249 @@
+gg.setVisible(false)
+  gg.clearResults()
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360725444,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360725460,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360725476,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360725572,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360725664,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360725492,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360726100,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360726104,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3360726128,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265160,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265164,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265168,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265172,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265176,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265180,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265184,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265188,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265192,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265196,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265200,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265204,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265208,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265212,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265216,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3109265220,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.getRangesList("libUE4.so")
+  gg.setValues({
+    [1] = {
+      address = 3361517940,
+      flags = 4,
+      value = 0
+    }
+  })
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+  gg.searchNumber(":com.tencent.ig", gg.TYPE_DWORD)
+  gg.getResults(67109377)
+  gg.setVisible(false)
+  gg.editAll("0", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+  gg.searchNumber(":com.pubg.krmobile", gg.TYPE_DWORD)
+  gg.getResults(67109377)
+  gg.setVisible(false)
+  gg.editAll("0", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+  gg.searchNumber(":com.vng.pubgmobile", gg.TYPE_DWORD)
+  gg.getResults(67109377)
+  gg.setVisible(false)
+  gg.editAll("0", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+  gg.searchNumber(":com.rekoo.pubgm", gg.TYPE_DWORD)
+  gg.getResults(67109377)
+  gg.setVisible(false)
+  gg.editAll("0", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+  gg.searchNumber(":com.pubg.imobile", gg.TYPE_DWORD)
+  gg.getResults(67109377)
+  gg.setVisible(false)
+  gg.editAll("0", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+  gg.searchNumber("1852142638", gg.TYPE_DWORD)
+  gg.refineNumber("1852142638", gg.TYPE_DWORD)
+  gg.getResults(67109377)
+  gg.setVisible(false)
+  gg.editAll("1852142691", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.alert("Bypass Logo Active")
